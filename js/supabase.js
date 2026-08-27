59fdfbedd40d7e6d6eccafb9549810832a79fdb0
one37349-create <one37349@gmail.com>
2026-08-27 17:07:34 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
 // RentoRide Supabase
+const SUPABASE_URL = "https://axvttcxrhsblvkmcnqgb.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_Nn7T1_HByXSy9Vrxy4XgoA_KlsJAafa";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
